12cf08b4593d294bec4089385da65d9247a03a0c
jackboys910 <george.jackboys@gmail.com>
2025-06-21 18:34:44 +0300

    chore: initialized the project, configured the work of linters

---
+++ app/screens/router.tsx
@@ -0,0 +1,7 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const App: React.FC = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>
+}
+
+export default App
---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./app'],
+        alias: {
+          '@components': './app/components',
+          '@store': './app/store',
+          '@constants': './app/constants',
+          '@hooks': './app/hooks',
+          '@utils': './app/utils',
+          '@interfaces': './app/interfaces',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+}
